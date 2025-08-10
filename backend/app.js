54e69c7eef948498238885cc689ad75a08a19102
Abhishek-kuliyal <80739363+Abhishek0827@users.noreply.github.com>
2025-08-10 18:04:40 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -104,7 +104,7 @@ const connectDB = async () => {
 };
 connectDB();
 
-const PORT = processed_messages.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("ok");
